185e95fae6464bff535d8dc4ceac0a69989cb3fc
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-03-11 13:57:32 +0100

    basic Dao and user entity

---
+++ jankocabek/src/main/java/dao/UserDao.java
@@ -0,0 +1,115 @@
+package dao;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import util.DbUtil;
+import entity.User;
+
+
+public class UserDao {
+    private static final String CREATE_USER_QUERY =
+            "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+    private static final String FIND_USER_QUERY =
+            "SELECT * FROM users WHERE id = ?";
+    private static final String UPDATE_USER_QUERY =
+            "UPDATE users SET username = ?, email = ?, password = ? WHERE  id = ?";
+    private static final String DELETE_USER_QUERY =
+            "DELETE FROM users WHERE id = ?";
+    private static final String FINDALL_USER_QUERY =
+            "SELECT * FROM users";
+    private static final String FIND_LAST_USER_QUERY =
+            "SELECT * FROM users ORDER BY id DESC LIMIT 1";
+
+    public User create(User user) {
+        try (Connection connection = DbUtil.getConnection()) {
+            final PreparedStatement ps = connection.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, user.getUserName());
+            ps.setString(2, user.getEmail());
+            ps.setString(3, user.getPassHash());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                user.setId(rs.getInt(1));
+            }
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace(System.err);
+            return null;
+        }
+    }
+
+    public User findById(int id) {
+        try (Connection connection = DbUtil.getConnection()) {
+            PreparedStatement ps = connection.prepareStatement(FIND_USER_QUERY);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return new User(rs.getInt(1),
+                        rs.getString("username"),
+                        rs.getString("email"),
+                        rs.getString("password"));
+            }
+            return null;
+        } catch (SQLException e) {
+            e.printStackTrace(System.err);
+            return null;
+        }
+    }
+
+    public void update(User user) {
+        try (Connection connection = DbUtil.getConnection()) {
+            PreparedStatement ps = connection.prepareStatement(UPDATE_USER_QUERY);
+            ps.setInt(4, user.getId());
+            ps.setString(1, user.getUserName());
+            ps.setString(2, user.getEmail());
+            ps.setString(3, user.getPassHash());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace(System.err);
+        }
+    }
+
+    public boolean delete(int userId) {
+        try (Connection connection = DbUtil.getConnection()) {
+            PreparedStatement ps = connection.prepareStatement(DELETE_USER_QUERY);
+            ps.setInt(1, userId);
+            return ps.executeUpdate() != 0;
+        } catch (SQLException e) {
+            e.printStackTrace(System.err);
+        }
+        return false;
+    }
+
+    public List<User> findAll() {
+        try (Connection connection = DbUtil.getConnection()) {
+            PreparedStatement ps = connection.prepareStatement(FINDALL_USER_QUERY);
+            ResultSet rs = ps.executeQuery();
+            List<User> users = new ArrayList<>();
+            while (rs.next()) {
+                users.add(new User(rs.getInt(1),
+                        rs.getString("username"),
+                        rs.getString("email"),
+                        rs.getString("password")));
+            }
+            return users;
+        } catch (SQLException e) {
+            e.printStackTrace(System.err);
+            return null;
+        }
+    }
+
+    public User findLast() {
+        try (Connection connection = DbUtil.getConnection()) {
+            PreparedStatement ps = connection.prepareStatement(FIND_LAST_USER_QUERY);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return new User(rs.getInt(1), rs.getString("username"), rs.getString("email"), rs.getString("password"));
+            }
+            return null;
+        } catch (SQLException e) {
+            e.printStackTrace(System.err);
+            return null;
+        }
+    }
+}
---
+++ jankocabek/src/main/java/entity/User.java
@@ -0,0 +1,92 @@
+package entity;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.util.Objects;
+
+public class User {
+    private int id;
+    private String userName;
+    private String passHash;
+    private String email;
+
+    public User(String userName, String email, String password) {
+        this.id = 0;
+        this.userName = userName;
+        this.email = email;
+        this.passHash = getHashedPass(password);
+    }
+
+    public User(int id, String userName, String email, String password) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+        this.passHash = getHashedPass(password);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassHash() {
+        return passHash;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.passHash = getHashedPass(password);
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private String getHashedPass(String password) {
+        return BCrypt.hashpw(password, generateSalt());
+    }
+
+    private String generateSalt() {
+        return BCrypt.gensalt();
+    }
+
+    public boolean checkPassword(String password) {
+        return BCrypt.checkpw(password, this.passHash);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", passHash='" + passHash + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return getId() == user.getId() && Objects.equals(getUserName(), user.getUserName()) && Objects.equals(getPassHash(), user.getPassHash()) && Objects.equals(getEmail(), user.getEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getUserName(), getPassHash(), getEmail());
+    }
+}
