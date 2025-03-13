bcf977514fbd1a76674ddc02d09dc04abc091563
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-03-13 13:48:05 +0100

    deleting debug stuff from AddUser.java

--- jankocabek/src/main/java/servlets/AddUser.java
+++ jankocabek/src/main/java/servlets/AddUser.java
@@ -1,6 +1,6 @@
 package servlets;
 
-import dao.UserDao;
+import userDao.UserDao;
 import entity.User;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -9,17 +9,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet("/user/add")
 public class AddUser extends HttpServlet {
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
-        out.println("<h1>get work</h1>");
-    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
