f961651ffebd08920a656923471f9ca9e433bcad
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-03-16 14:41:37 +0100

    fixing DeleteUser.java Servlet. -deleting unnecessary session changing from get into post because delete userId is sent by form

--- jankocabek/src/main/java/servlets/DeleteUser.java
+++ jankocabek/src/main/java/servlets/DeleteUser.java
@@ -1,12 +1,11 @@
 package servlets;
 
+import dao.UserDao;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-import dao.UserDao;
 
 import java.io.IOException;
 
@@ -14,12 +13,10 @@ import java.io.IOException;
 public class DeleteUser extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         int id = Integer.parseInt(req.getParameter("userId"));
-        HttpSession session = req.getSession();
-        session.setAttribute("userId", id);
         UserDao userDao = new UserDao();
         userDao.delete(id);
-        resp.sendRedirect(req.getContextPath() + "/");
+        resp.sendRedirect(req.getContextPath() + "/main");
     }
 }
