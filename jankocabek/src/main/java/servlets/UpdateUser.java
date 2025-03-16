ba348d5d231647aa47cc68bc8173d35b80f2ffe0
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-03-16 20:12:18 +0100

    Update for functions without needs to putting password in

--- jankocabek/src/main/java/servlets/UpdateUser.java
+++ jankocabek/src/main/java/servlets/UpdateUser.java
@@ -2,7 +2,6 @@ package servlets;
 
 import dao.UserDao;
 import entity.User;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,14 +13,18 @@ import java.io.IOException;
 public class UpdateUser extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         UserDao userDao = new UserDao();
         String username = req.getParameter("userName");
         String email = req.getParameter("email");
-        String password = req.getParameter("password");
         int id = Integer.parseInt(req.getParameter("id"));
-
-        userDao.update(new User(id, username,email,password));
+        String password;
+        if (req.getParameter("password") == null || req.getParameter("password").isEmpty()) {
+            userDao.updateWithoutPassword(new User(id, username, email));
+        } else {
+            password = req.getParameter("password");
+            userDao.update(new User(id, username, email, password));
+        }
         resp.sendRedirect(req.getContextPath() + "/main");
     }
 }
