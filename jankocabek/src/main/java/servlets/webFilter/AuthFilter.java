cbadbec27f5de53b49b98c7e492e56eeac30e178
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-03-16 14:45:09 +0100

    adding AuthFilter.java for disallowed access without a login

---
+++ jankocabek/src/main/java/servlets/webFilter/AuthFilter.java
@@ -0,0 +1,32 @@
+package servlets.webFilter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class AuthFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpSession session = httpRequest.getSession(false);
+        String uri = httpRequest.getRequestURI();
+
+        if (uri.startsWith("/login")) {
+            chain.doFilter(request, response);
+            return;
+        }
+        // Check if the username attribute exists in the session
+        if (session != null && session.getAttribute("username") != null) {
+           chain.doFilter(request, response); // User is logged in, proceed
+        } else {
+            // Redirect to login page
+            httpResponse.sendRedirect("/login");
+        }
+    }
+}
